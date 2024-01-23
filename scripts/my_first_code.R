34858be8f1fc471a592443d6371d694ee8d8a3d9
alicenev1 <a.nevone@smatteo.pv.it>
2024-01-23 15:01:44 +0100

    my first R script

---
+++ scripts/my_first_code.R
@@ -0,0 +1,12 @@
+3 + 4
+3+4
+weight_kg <- 55 
+
+Weight_kg <- 34
+my_weight_kg <- 50
+2.2 * weight_kg
+weight_kg <- 57
+2.2 * weight_kg
+weight_lb <- 2.2 * weight_kg
+weight_kg <- 100
+weight_lb <- 2.2 * weight_kg
