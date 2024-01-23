5b4277892e7bd1c2aa9f9fd791acea1685415c9a
alicenev1 <a.nevone@smatteo.pv.it>
2024-01-23 18:28:22 +0100

    Starting working with data frame

--- scripts/my_first_code.R
+++ scripts/my_first_code.R
@@ -8,5 +8,27 @@ my_weight_kg <- 50
 weight_kg <- 57
 2.2 * weight_kg
 weight_lb <- 2.2 * weight_kg
+# I have to update the value weight_lb object
 weight_kg <- 100
+
 weight_lb <- 2.2 * weight_kg
+
+weight_kg
+weight_lb
+
+mass <- 47.5
+age <- 122
+
+mass <- mass * 2
+age <- age - 20
+
+mass_index <- mass/age
+sqrt(9)
+weight_kg <- sqrt(9)
+round(3.14159)
+round(3.14159, digit = 2)
+round(3.14159, 2)
+round(digits = 2, x=3.14159)
+my_function <- function (x){x <- x + 2}
+  
+my_result <- my_function(3)
